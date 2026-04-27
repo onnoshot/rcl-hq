27f86ee0f3cb285855e1fdefc315a7629cfa77c6
Onur Oğuz <onnoshot@192.168.1.4>
2026-04-27 03:44:42 +0300

    init: RCL HQ Dashboard — Shopify + YouTube + Instagram

---
+++ data.js
@@ -0,0 +1,542 @@
+/* ─── YOUTUBE DATA START ─── */
+const YOUTUBE = {
+  "updated_at": "2026-04-27T02:51:05",
+  "channel": {
+    "title": "Retro Camera Land",
+    "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
+    "subscribers": 3750,
+    "total_views": 1041275,
+    "video_count": 370
+  },
+  "analytics": {
+    "last_30d": {
+      "views": 125871,
+      "watch_hours": 482.6,
+      "subs_gained": 86,
+      "subs_lost": 55,
+      "avg_view_sec": 34
+    },
+    "monthly": [
+      {
+        "label": "Kas 25",
+        "views": 49291,
+        "watch_min": 4313,
+        "subs_gained": 323
+      },
+      {
+        "label": "Ara 25",
+        "views": 102641,
+        "watch_min": 16916,
+        "subs_gained": 437
+      },
+      {
+        "label": "Oca 26",
+        "views": 214842,
+        "watch_min": 45223,
+        "subs_gained": 2739
+      },
+      {
+        "label": "Şub 26",
+        "views": 119281,
+        "watch_min": 20493,
+        "subs_gained": 831
+      },
+      {
+        "label": "Mar 26",
+        "views": 234232,
+        "watch_min": 39277,
+        "subs_gained": 337
+      },
+      {
+        "label": "Nis 26",
+        "views": 82462,
+        "watch_min": 18136,
+        "subs_gained": 64
+      }
+    ]
+  },
+  "videos": [
+    {
+      "id": "bY4UWZ-wEwY",
+      "title": "Nikon Coolpix S52C (2008) RetroCameraLand’de! #nikon #coolpi",
+      "date": "26 Nis 2026",
+      "views": 655,
+      "likes": 0,
+      "comments": 0,
+      "url": "https://youtu.be/bY4UWZ-wEwY"
+    },
+    {
+      "id": "-mrJ8maR2eo",
+      "title": "Canon PowerShot SD400: 2026'da Herkesin Peşinde! Y2K Dijital",
+      "date": "25 Nis 2026",
+      "views": 49,
+      "likes": 8,
+      "comments": 3,
+      "url": "https://youtu.be/-mrJ8maR2eo"
+    },
+    {
+      "id": "ie_8TBsWv-Y",
+      "title": "Dijital kamera ile fotoğraf çektirme sorunsalı 🤣 #digitalcam",
+      "date": "20 Nis 2026",
+      "views": 2217,
+      "likes": 19,
+      "comments": 0,
+      "url": "https://youtu.be/ie_8TBsWv-Y"
+    },
+    {
+      "id": "8oUm3q_y6rQ",
+      "title": "Nikon Coolpix S6900 💘 Kolleksiyonluk Y2K Digicam #nikon #coo",
+      "date": "18 Nis 2026",
+      "views": 2590,
+      "likes": 26,
+      "comments": 0,
+      "url": "https://youtu.be/8oUm3q_y6rQ"
+    },
+    {
+      "id": "mUJq-SQWXX8",
+      "title": "20 yıllık bir kamerayla Amsterdam 📸✨ #Amsterdam #SonyCyberSh",
+      "date": "10 Nis 2026",
+      "views": 2013,
+      "likes": 23,
+      "comments": 1,
+      "url": "https://youtu.be/mUJq-SQWXX8"
+    },
+    {
+      "id": "zvmyHAaT9cc",
+      "title": "Canon Ixus 160 Mirror Selfie 📸 #canon #y2k #digitalcamera #r",
+      "date": "1 Nis 2026",
+      "views": 2066,
+      "likes": 8,
+      "comments": 0,
+      "url": "https://youtu.be/zvmyHAaT9cc"
+    },
+    {
+      "id": "Q3yhoMup7hg",
+      "title": "Canon Ixus 160 | Dijital Kamera Fotoğraf ve Video İnceleme 🔻",
+      "date": "1 Nis 2026",
+      "views": 586,
+      "likes": 7,
+      "comments": 0,
+      "url": "https://youtu.be/Q3yhoMup7hg"
+    },
+    {
+      "id": "To_AIAUFESk",
+      "title": "Canon IXUS 160: Küçük Kamera, Büyük Sürpriz",
+      "date": "31 Mar 2026",
+      "views": 213,
+      "likes": 11,
+      "comments": 1,
+      "url": "https://youtu.be/To_AIAUFESk"
+    },
+    {
+      "id": "p6nrJUlbC0I",
+      "title": "Canon IXUS I 📸 İkonik Dijital Fotoğraf Makinesi 🥰 #canon #ix",
+      "date": "28 Mar 2026",
+      "views": 2090,
+      "likes": 9,
+      "comments": 0,
+      "url": "https://youtu.be/p6nrJUlbC0I"
+    },
+    {
+      "id": "d2jR-N8nVWM",
+      "title": "Canon IXUS I 📸 Efsane Dijital Fotoğraf Makinesi ❤️‍🔥 #canon ",
+      "date": "27 Mar 2026",
+      "views": 1221,
+      "likes": 9,
+      "comments": 0,
+      "url": "https://youtu.be/d2jR-N8nVWM"
+    }
+  ],
+  "sub_goal": 10000
+};
+/* ─── YOUTUBE DATA END ─── */
+
+/* ─── SHOPIFY DATA START ─── */
+const SHOPIFY = {
+  "updated_at": "2026-04-27T02:48:08",
+  "period_30d": {
+    "revenue": 57330,
+    "orders": 5,
+    "aov": 11466
+  },
+  "period_90d": {
+    "revenue": 143250,
+    "orders": 12,
+    "aov": 11938
+  },
+  "period_year": {
+    "revenue": 693145,
+    "orders": 58,
+    "aov": 11951
+  },
+  "customers_total": 676,
+  "monthly_labels": [
+    "May 25",
+    "Haz 25",
+    "Tem 25",
+    "Ağu 25",
+    "Eyl 25",
+    "Eki 25",
+    "Kas 25",
+    "Ara 25",
+    "Oca 26",
+    "Oca 26",
+    "Mar 26",
+    "Nis 26"
+  ],
+  "monthly_revenue": [
+    0,
+    0,
+    2590,
+    68639,
+    62128,
+    82649,
+    233639,
+    43360,
+    56890,
+    56890,
+    46450,
+    57330
+  ],
+  "monthly_orders": [
+    0,
+    0,
+    1,
+    6,
+    9,
+    8,
+    12,
+    4,
+    6,
+    6,
+    4,
+    5
+  ],
+  "channels": [
+    {
+      "name": "Direkt",
+      "icon": "🔗",
+      "color": "#F5A623",
+      "orders": 23,
+      "rev": 305820,
+      "order_pct": 39.7,
+      "rev_pct": 44.1
+    },
+    {
+      "name": "Google",
+      "icon": "🔍",
+      "color": "#4285F4",
+      "orders": 19,
+      "rev": 216176,
+      "order_pct": 32.8,
+      "rev_pct": 31.2
+    },
+    {
+      "name": "Instagram",
+      "icon": "📸",
+      "color": "#BF5AF2",
+      "orders": 14,
+      "rev": 151919,
+      "order_pct": 24.1,
+      "rev_pct": 21.9
+    },
+    {
+      "name": "YouTube",
+      "icon": "▶",
+      "color": "#FF453A",
+      "orders": 1,
+      "rev": 9990,
+      "order_pct": 1.7,
+      "rev_pct": 1.4
+    },
+    {
+      "name": "AI / Arama",
+      "icon": "🤖",
+      "color": "#5AC8FA",
+      "orders": 1,
+      "rev": 9240,
+      "order_pct": 1.7,
+      "rev_pct": 1.3
+    }
+  ],
+  "recent_orders": [
+    {
+      "date": "26 Nis 2026",
+      "amount": 9990
+    },
+    {
+      "date": "18 Nis 2026",
+      "amount": 8490
+    },
+    {
+      "date": "17 Nis 2026",
+      "amount": 14490
+    },
+    {
+      "date": "10 Nis 2026",
+      "amount": 12490
+    },
+    {
+      "date": "3 Nis 2026",
+      "amount": 11870
+    },
+    {
+      "date": "26 Mar 2026",
+      "amount": 9990
+    },
+    {
+      "date": "20 Mar 2026",
+      "amount": 11480
+    },
+    {
+      "date": "9 Mar 2026",
+      "amount": 13490
+    },
+    {
+      "date": "8 Mar 2026",
+      "amount": 11490
+    },
+    {
+      "date": "27 Şub 2026",
+      "amount": 9990
+    }
+  ],
+  "cameras": [
+    {
+      "name": "Sony Cyber-shot DSC-TX9",
+      "price": 14490
+    },
+    {
+      "name": "Canon PowerShot SD400",
+      "price": 12490
+    },
+    {
+      "name": "Casio Exilim EX-N10 Gold Edition",
+      "price": 11990
+    },
+    {
+      "name": "Lumix DMC-FT10",
+      "price": 11990
+    },
+    {
+      "name": "Casio Exilim EX-Z110",
+      "price": 11490
+    },
+    {
+      "name": "Fujifilm FinePix 4700 Zoom",
+      "price": 11490
+    },
+    {
+      "name": "Fujifilm FinePix T200",
+      "price": 11490
+    },
+    {
+      "name": "Nikon Coolpix S52c",
+      "price": 11490
+    },
+    {
+      "name": "Olympus SP-700",
+      "price": 11490
+    },
+    {
+      "name": "Konica Minolta DiMAGE X50",
+      "price": 10990
+    },
+    {
+      "name": "JVC GZ-MC200E",
+      "price": 10490
+    },
+    {
+      "name": "Kodak EasyShare V603",
+      "price": 10490
+    },
+    {
+      "name": "Kodak Slice R502",
+      "price": 10490
+    },
+    {
+      "name": "Sanyo Xacti VPC-HD1",
+      "price": 10490
+    },
+    {
+      "name": "Casio Exilim EX-Z4",
+      "price": 9490
+    },
+    {
+      "name": "Casio Exilim EX-Z85",
+      "price": 9490
+    },
+    {
+      "name": "Olympus SP-320",
+      "price": 9490
+    },
+    {
+      "name": "Olympus SZ-10",
+      "price": 9490
+    },
+    {
+      "name": "HP Photosmart R827",
+      "price": 8900
+    },
+    {
+      "name": "Nikon Coolpix 885",
+      "price": 8900
+    },
+    {
+      "name": "Traveler DC-830",
+      "price": 8490
+    },
+    {
+      "name": "Sony Cybershot DSC-S85",
+      "price": 7990
+    },
+    {
+      "name": "Kodak ZX1 “Weather-Resistant”",
+      "price": 4900
+    }
+  ],
+  "accessories": [
+    {
+      "name": "Type-C ve USB 2.0 SD-MicroSD TF Kart Okuyucu",
+      "price": 1490,
+      "qty": 3
+    },
+    {
+      "name": "Y2K Digicam Fotoğraf/Video Aktarıcı - xD, CF, SD, MS Destekli All-in-One Kart Okuyucu",
+      "price": 790,
+      "qty": 2
+    },
+    {
+      "name": "USB-C 3'ü 1 Arada Kart Okuyucu — SD / MicroSD / USB",
+      "price": 690,
+      "qty": 2
+    },
+    {
+      "name": "Universal Kamera Batarya Şarj Cihazı",
+      "price": 590,
+      "qty": 2
+    },
+    {
+      "name": "Ulanzi VLOG Tripod",
+      "price": 990,
+      "qty": 1
+    }
+  ],
+  "out_of_stock": 76
+};
+/* ─── SHOPIFY DATA END ─── */
+
+/* ─── INSTAGRAM DATA START ─── */
+const INSTAGRAM = {
+  "updated_at": "2026-04-27T03:29:25",
+  "username": "retrocameraland",
+  "name": "Retro Camera Land",
+  "followers": 5703,
+  "media_count": 264,
+  "avg_likes": 34,
+  "total_engagement": 409,
+  "type_counts": {
+    "VIDEO": 5,
+    "CAROUSEL_ALBUM": 7
+  },
+  "posts": [
+    {
+      "id": "18105347032902803",
+      "date": "26 Nis 2026",
+      "type": "VIDEO",
+      "likes": 27,
+      "comments": 0,
+      "url": "https://www.instagram.com/reel/DXltdQ7IPMH/"
+    },
+    {
+      "id": "18089660810248856",
+      "date": "25 Nis 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 24,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DXjGTiHiCAA/"
+    },
+    {
+      "id": "18086971511203373",
+      "date": "20 Nis 2026",
+      "type": "VIDEO",
+      "likes": 15,
+      "comments": 1,
+      "url": "https://www.instagram.com/reel/DXXAHRCI7gE/"
+    },
+    {
+      "id": "18042760670777322",
+      "date": "19 Nis 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 23,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DXUL5lEiOjj/"
+    },
+    {
+      "id": "17948676963137020",
+      "date": "18 Nis 2026",
+      "type": "VIDEO",
+      "likes": 103,
+      "comments": 0,
+      "url": "https://www.instagram.com/reel/DXR-eNPCCkE/"
+    },
+    {
+      "id": "17965682838063532",
+      "date": "14 Nis 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 37,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DXHmampiN8B/"
+    },
+    {
+      "id": "18555755458064956",
+      "date": "10 Nis 2026",
+      "type": "VIDEO",
+      "likes": 34,
+      "comments": 0,
+      "url": "https://www.instagram.com/reel/DW8mL3tDfxe/"
+    },
+    {
+      "id": "17944368594155820",
+      "date": "7 Nis 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 39,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DW0_9OACK9U/"
+    },
+    {
+      "id": "18095278277320639",
+      "date": "2 Nis 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 34,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DWorCleiNkY/"
+    },
+    {
+      "id": "17960766579062733",
+      "date": "1 Nis 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 18,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DWmBmcTiHBa/"
+    },
+    {
+      "id": "18415866235193299",
+      "date": "1 Nis 2026",
+      "type": "VIDEO",
+      "likes": 27,
+      "comments": 0,
+      "url": "https://www.instagram.com/reel/DWlWvR0Iu4Q/"
+    },
+    {
+      "id": "18097648411818660",
+      "date": "26 Mar 2026",
+      "type": "CAROUSEL_ALBUM",
+      "likes": 27,
+      "comments": 0,
+      "url": "https://www.instagram.com/p/DWW5d3fCBrx/"
+    }
+  ],
+  "follower_goal": 10000
+};
+/* ─── INSTAGRAM DATA END ─── */
