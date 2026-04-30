3a0aeccfb0375ffdace0efd849b455a5a054e679
Onur Oğuz <onnoshot@192.168.1.4>
2026-04-30 13:05:34 +0300

    data: youtube 2026-04-30T13:05

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-04-30T12:28:14",
+  "updated_at": "2026-04-30T13:05:34",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -60,7 +60,7 @@ const YOUTUBE = {
       "id": "UcdfytnpKEA",
       "title": "Fujifilm Dijital Kamera ile Günlük Fotoğraflar Çek 😍📸 #fujif",
       "date": "29 Nis 2026",
-      "views": 1347,
+      "views": 1404,
       "likes": 17,
       "comments": 0,
       "url": "https://youtu.be/UcdfytnpKEA"
@@ -105,8 +105,8 @@ const YOUTUBE = {
       "id": "ie_8TBsWv-Y",
       "title": "Dijital kamera ile fotoğraf çektirme sorunsalı 🤣 #digitalcam",
       "date": "20 Nis 2026",
-      "views": 2313,
-      "likes": 20,
+      "views": 2437,
+      "likes": 21,
       "comments": 1,
       "url": "https://youtu.be/ie_8TBsWv-Y"
     },
@@ -114,7 +114,7 @@ const YOUTUBE = {
       "id": "8oUm3q_y6rQ",
       "title": "Nikon Coolpix S6900 💘 Kolleksiyonluk Y2K Digicam #nikon #coo",
       "date": "18 Nis 2026",
-      "views": 2819,
+      "views": 2898,
       "likes": 33,
       "comments": 0,
       "url": "https://youtu.be/8oUm3q_y6rQ"
