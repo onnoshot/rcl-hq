e2ba39af393d11c5c7fdef19a2fe14aa473dfcac
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-01 10:06:48 +0300

    data: youtube 2026-05-01T10:06

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-05-01T09:29:21",
+  "updated_at": "2026-05-01T10:06:48",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -54,8 +54,8 @@ const YOUTUBE = {
       "id": "3qQFPZtOHf0",
       "title": "VAPURDA CANIN SIKILIRSA… Kameranla Anlar Yakala 📸 #dijitalka",
       "date": "30 Nis 2026",
-      "views": 2130,
-      "likes": 10,
+      "views": 2134,
+      "likes": 9,
       "comments": 0,
       "url": "https://youtu.be/3qQFPZtOHf0"
     },
@@ -63,7 +63,7 @@ const YOUTUBE = {
       "id": "UcdfytnpKEA",
       "title": "Fujifilm Dijital Kamera ile Günlük Fotoğraflar Çek 😍📸 #fujif",
       "date": "29 Nis 2026",
-      "views": 2323,
+      "views": 2422,
       "likes": 17,
       "comments": 0,
       "url": "https://youtu.be/UcdfytnpKEA"
@@ -109,7 +109,7 @@ const YOUTUBE = {
       "title": "Dijital kamera ile fotoğraf çektirme sorunsalı 🤣 #digitalcam",
       "date": "20 Nis 2026",
       "views": 2834,
-      "likes": 22,
+      "likes": 24,
       "comments": 2,
       "url": "https://youtu.be/ie_8TBsWv-Y"
     },
