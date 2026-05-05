0053c8e3a98e349496f5c0b17e3d70fbe99d494b
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-05 03:39:12 +0300

    data: youtube 2026-05-05T03:39

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-05-05T02:39:10",
+  "updated_at": "2026-05-05T03:39:12",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,8 +63,8 @@ const YOUTUBE = {
       "id": "3qQFPZtOHf0",
       "title": "VAPURDA CANIN SIKILIRSA… Kameranla Anlar Yakala 📸 #dijitalka",
       "date": "30 Nis 2026",
-      "views": 2275,
-      "likes": 22,
+      "views": 2281,
+      "likes": 23,
       "comments": 0,
       "url": "https://youtu.be/3qQFPZtOHf0"
     },
