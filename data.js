0289de8f18aaa12c118e628712a2b3e3610b53d5
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-08 02:53:06 +0300

    data: youtube 2026-05-08T02:53

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-05-08T01:53:04",
+  "updated_at": "2026-05-08T02:53:06",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,7 +63,7 @@ const YOUTUBE = {
       "id": "VnLTp2ufa6A",
       "title": "Canon IXUS (2001) Muhteşem Fotoğraflar 📸",
       "date": "7 May 2026",
-      "views": 1254,
+      "views": 1300,
       "likes": 21,
       "comments": 1,
       "url": "https://youtu.be/VnLTp2ufa6A"
