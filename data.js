65a865219433418ef2f0960d911f109463a49d84
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-08 17:37:42 +0300

    data: youtube 2026-05-08T17:37

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-05-08T17:37:41",
+  "updated_at": "2026-05-08T17:37:42",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,7 +63,7 @@ const YOUTUBE = {
       "id": "VnLTp2ufa6A",
       "title": "Canon IXUS (2001) Muhteşem Fotoğraflar 📸",
       "date": "7 May 2026",
-      "views": 1342,
+      "views": 1338,
       "likes": 21,
       "comments": 1,
       "url": "https://youtu.be/VnLTp2ufa6A"
@@ -72,7 +72,7 @@ const YOUTUBE = {
       "id": "yIrL45HroS8",
       "title": "Canon IXUS (2001) Küçük Retro  ile Vapur Turu 📸",
       "date": "6 May 2026",
-      "views": 1250,
+      "views": 1251,
       "likes": 9,
       "comments": 0,
       "url": "https://youtu.be/yIrL45HroS8"
