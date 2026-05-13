a3b1af12ef07be40101767ef1b6f8a299438a63c
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-13 03:44:53 +0300

    data: instagram 2026-05-13T03:44

--- data.js
+++ data.js
@@ -431,13 +431,13 @@ const SHOPIFY = {
 
 /* ─── INSTAGRAM DATA START ─── */
 const INSTAGRAM = {
-  "updated_at": "2026-05-13T02:44:49",
+  "updated_at": "2026-05-13T03:44:53",
   "username": "retrocameraland",
   "name": "Retro Camera Land",
   "followers": 6241,
   "media_count": 271,
   "avg_likes": 58,
-  "total_engagement": 702,
+  "total_engagement": 704,
   "type_counts": {
     "CAROUSEL_ALBUM": 7,
     "VIDEO": 5
@@ -447,7 +447,7 @@ const INSTAGRAM = {
       "id": "18111928819850525",
       "date": "12 May 2026",
       "type": "CAROUSEL_ALBUM",
-      "likes": 107,
+      "likes": 109,
       "comments": 0,
       "url": "https://www.instagram.com/p/DYO7S-2iEqR/"
     },
