079933d243bf00e4fcb1be7bb0f6fe09482cc7dc
Onur Oğuz <onnoshot@Onur-MacBook-Pro.local>
2026-05-15 14:30:25 +0300

    data: youtube 2026-05-15T14:30

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-05-15T09:28:47",
+  "updated_at": "2026-05-15T14:30:25",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
-    "subscribers": 3970,
+    "subscribers": 3980,
     "total_views": 1116685,
     "video_count": 379
   },
@@ -63,8 +63,8 @@ const YOUTUBE = {
       "id": "TvPGsbRhSmQ",
       "title": "Fujifilm Finepix Z700 EXR | Muhteşem Retro Kamera #y2k #digi",
       "date": "14 May 2026",
-      "views": 1416,
-      "likes": 16,
+      "views": 1516,
+      "likes": 18,
       "comments": 0,
       "url": "https://youtu.be/TvPGsbRhSmQ"
     },
@@ -72,8 +72,8 @@ const YOUTUBE = {
       "id": "LHg76IQG7aw",
       "title": "Fujifilm Finepix Z90 | İKONİK PEMBE DİJİTAL KAMERAYA AŞIK OL",
       "date": "13 May 2026",
-      "views": 1151,
-      "likes": 13,
+      "views": 1182,
+      "likes": 15,
       "comments": 0,
       "url": "https://youtu.be/LHg76IQG7aw"
     },
@@ -108,8 +108,8 @@ const YOUTUBE = {
       "id": "3qQFPZtOHf0",
       "title": "VAPURDA CANIN SIKILIRSA… Kameranla Anlar Yakala 📸 #dijitalka",
       "date": "30 Nis 2026",
-      "views": 4553,
-      "likes": 77,
+      "views": 4644,
+      "likes": 81,
       "comments": 0,
       "url": "https://youtu.be/3qQFPZtOHf0"
     },
@@ -135,7 +135,7 @@ const YOUTUBE = {
       "id": "U0c_YNzbRHE",
       "title": "Fujifilm Finepix Z110 | Original Special Edition Color 💘 #fu",
       "date": "29 Nis 2026",
-      "views": 581,
+      "views": 582,
       "likes": 7,
       "comments": 0,
       "url": "https://youtu.be/U0c_YNzbRHE"
