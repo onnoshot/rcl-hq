5ab75e172479c231e23d76ef33e85e8f81a39296
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-19 02:45:07 +0300

    data: shopify 2026-05-19T02:45

--- data.js
+++ data.js
@@ -156,7 +156,7 @@ const YOUTUBE = {
 
 /* ─── SHOPIFY DATA START ─── */
 const SHOPIFY = {
-  "updated_at": "2026-05-19T02:44:56",
+  "updated_at": "2026-05-19T02:45:07",
   "period_30d": {
     "revenue": 125550,
     "orders": 10,
