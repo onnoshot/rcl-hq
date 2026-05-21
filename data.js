0dc642876129ef326237b766fba899adfa14e3b1
Onur Oğuz <onnoshot@192.168.1.4>
2026-05-21 15:50:13 +0300

    data: youtube 2026-05-21T15:50

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-05-21T15:50:11",
+  "updated_at": "2026-05-21T15:50:13",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,7 +63,7 @@ const YOUTUBE = {
       "id": "GULk4atiPqo",
       "title": "kaydet. sakla. hatırla 💌#digitalcamera #y2k #retroaesthetic",
       "date": "17 May 2026",
-      "views": 396,
+      "views": 397,
       "likes": 3,
       "comments": 0,
       "url": "https://youtu.be/GULk4atiPqo"
