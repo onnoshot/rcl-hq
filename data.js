f4cc79be30d60674d2562b28e82980819d55f557
Onur Oğuz <onnoshot@192.168.1.4>
2026-06-01 15:28:27 +0300

    data: instagram 2026-06-01T15:28

--- data.js
+++ data.js
@@ -417,7 +417,7 @@ const SHOPIFY = {
 
 /* ─── INSTAGRAM DATA START ─── */
 const INSTAGRAM = {
-  "updated_at": "2026-06-01T14:28:25",
+  "updated_at": "2026-06-01T15:28:27",
   "username": "retrocameraland",
   "name": "Retro Camera Land",
   "followers": 6823,
