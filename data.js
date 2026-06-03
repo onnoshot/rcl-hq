7f84d0de7c6492e6cfe1c9d4bc31a7893475a014
Onur Oğuz <onnoshot@192.168.1.4>
2026-06-03 03:11:53 +0300

    data: youtube 2026-06-03T03:11

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-06-03T03:11:52",
+  "updated_at": "2026-06-03T03:11:53",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -84,7 +84,7 @@ const YOUTUBE = {
       "id": "TvPGsbRhSmQ",
       "title": "Fujifilm Finepix Z700 EXR | Muhteşem Retro Kamera #y2k #digi",
       "date": "14 May 2026",
-      "views": 2792,
+      "views": 2798,
       "likes": 42,
       "comments": 0,
       "url": "https://youtu.be/TvPGsbRhSmQ"
