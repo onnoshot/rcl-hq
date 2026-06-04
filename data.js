257935d2d3394ccdee71f793751f5515771054b8
Onur Oğuz <onnoshot@Onur-MacBook-Pro.local>
2026-06-04 21:57:46 +0300

    data: youtube 2026-06-04T21:57

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-06-04T21:57:44",
+  "updated_at": "2026-06-04T21:57:46",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,7 +63,7 @@ const YOUTUBE = {
       "id": "HnGR92gHNDY",
       "title": "MUHTEŞEM ANLARI DİJİTAL KAMERAN İLE KAYDET 📸🥂",
       "date": "4 Haz 2026",
-      "views": 461,
+      "views": 464,
       "likes": 7,
       "comments": 0,
       "url": "https://youtu.be/HnGR92gHNDY"
