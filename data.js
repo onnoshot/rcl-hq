1008c7d53d1be40bcf0c9f62509505fd86f6ef0e
Onur Oğuz <onnoshot@192.168.1.5>
2026-06-06 10:02:47 +0300

    data: youtube 2026-06-06T10:02

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-06-06T10:02:46",
+  "updated_at": "2026-06-06T10:02:47",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -72,7 +72,7 @@ const YOUTUBE = {
       "id": "2r7TMYA4kTw",
       "title": "Renklerin peşinde bir yolculuk 📸 #fujifilm #retro #digitalca",
       "date": "22 May 2026",
-      "views": 217,
+      "views": 218,
       "likes": 5,
       "comments": 1,
       "url": "https://youtu.be/2r7TMYA4kTw"
