f2a49ae2a6bc3c98b941d8672f18310f057ef79d
Onur Oğuz <onnoshot@192.168.1.5>
2026-06-06 23:54:59 +0300

    data: youtube 2026-06-06T23:54

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-06-06T22:54:55",
+  "updated_at": "2026-06-06T23:54:58",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
