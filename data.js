8d5c48409b439dc93413a630c2b8bcd21c977fe7
Onur Oğuz <onnoshot@Onur-MacBook-Pro.local>
2026-06-13 16:43:40 +0300

    data: shopify 2026-06-13T16:43

--- data.js
+++ data.js
@@ -156,7 +156,7 @@ const YOUTUBE = {
 
 /* ─── SHOPIFY DATA START ─── */
 const SHOPIFY = {
-  "updated_at": "2026-06-13T15:43:36",
+  "updated_at": "2026-06-13T16:43:40",
   "period_30d": {
     "revenue": 129599,
     "orders": 11,
