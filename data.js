2cf2d4288f6c9207c03d07d1538b0bbb527d7a21
onnoshot <onurxvala@gmail.com>
2026-06-25 16:15:23 +0300

    data: youtube 2026-06-25T16:15

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-06-25T15:15:19",
+  "updated_at": "2026-06-25T16:15:23",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -65,7 +65,7 @@ const YOUTUBE = {
       "date": "20 Haz 2026",
       "views": 1734,
       "likes": 18,
-      "comments": 0,
+      "comments": 1,
       "url": "https://youtu.be/8XWAJXORa9c"
     },
     {
