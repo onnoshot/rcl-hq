d107044cba8354cb54809f1cc8876f6b37594907
onnoshot <onurxvala@gmail.com>
2026-07-02 02:20:55 +0300

    data: youtube 2026-07-02T02:20

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-07-02T01:20:49",
+  "updated_at": "2026-07-02T02:20:54",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -138,8 +138,8 @@ const YOUTUBE = {
       "id": "LHg76IQG7aw",
       "title": "Fujifilm Finepix Z90 | İKONİK PEMBE DİJİTAL KAMERAYA AŞIK OL",
       "date": "13 May 2026",
-      "views": 1784,
-      "likes": 28,
+      "views": 1785,
+      "likes": 29,
       "comments": 1,
       "url": "https://youtu.be/LHg76IQG7aw"
     }
