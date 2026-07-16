cc51c8a57c9b30cca58235fd09726422f466b13f
onnoshot <onurxvala@gmail.com>
2026-07-16 23:52:20 +0300

    data: youtube 2026-07-16T23:52

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-07-16T22:52:14",
+  "updated_at": "2026-07-16T23:52:19",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,8 +63,8 @@ const YOUTUBE = {
       "id": "4mC6-c_-JYc",
       "title": "Kompakt Canon Dijital Kamera ile Manzara Fotoğrafçılığı 📸",
       "date": "16 Tem 2026",
-      "views": 1050,
-      "likes": 7,
+      "views": 1446,
+      "likes": 8,
       "comments": 0,
       "url": "https://youtu.be/4mC6-c_-JYc"
     },
