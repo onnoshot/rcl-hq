4fb4d3fdeff40d3fe05b3c08d7a6c502944d0516
onnoshot <onurxvala@gmail.com>
2026-07-20 19:05:24 +0300

    data: shopify 2026-07-20T19:05

--- data.js
+++ data.js
@@ -156,7 +156,7 @@ const YOUTUBE = {
 
 /* ─── SHOPIFY DATA START ─── */
 const SHOPIFY = {
-  "updated_at": "2026-07-20T18:05:16",
+  "updated_at": "2026-07-20T19:05:23",
   "catalog": [
     {
       "handle": "agfa-optima-104",
