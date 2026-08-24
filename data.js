80f840c8e423aed4940307b0de962e1ed0fd3f45
onnoshot <onurxvala@gmail.com>
2026-08-24 23:25:47 +0300

    data: shopify 2026-08-24T23:25

--- data.js
+++ data.js
@@ -156,7 +156,7 @@ const YOUTUBE = {
 
 /* ─── SHOPIFY DATA START ─── */
 const SHOPIFY = {
-  "updated_at": "2026-08-24T22:25:40",
+  "updated_at": "2026-08-24T23:25:46",
   "period_30d": {
     "revenue": 68920,
     "orders": 7,
