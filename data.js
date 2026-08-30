37774b27ccb087ab0b1eb9192550fd75afc59d93
onnoshot <onurxvala@gmail.com>
2026-08-30 11:02:04 +0300

    data: youtube 2026-08-30T11:02

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-08-30T10:01:59",
+  "updated_at": "2026-08-30T11:02:04",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -72,7 +72,7 @@ const YOUTUBE = {
       "id": "pZJB6mBWYRg",
       "title": "Samsung WB350F İncelemesi: 21x Zoom Yapan Retro Dijital Kame",
       "date": "16 Tem 2026",
-      "views": 2760,
+      "views": 2762,
       "likes": 16,
       "comments": 1,
       "url": "https://youtu.be/pZJB6mBWYRg"
