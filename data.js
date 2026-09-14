23fce6cc1e3bcf232f8311fc7c134b6eb4f1206f
onnoshot <onurxvala@gmail.com>
2026-09-14 19:41:12 +0300

    data: youtube 2026-09-14T19:41

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* ─── YOUTUBE DATA START ─── */
 const YOUTUBE = {
-  "updated_at": "2026-09-14T18:41:05",
+  "updated_at": "2026-09-14T19:41:11",
   "channel": {
     "title": "Retro Camera Land",
     "channel_id": "UCq0jJ7knS1MDtNgx8DtJCvw",
@@ -63,7 +63,7 @@ const YOUTUBE = {
       "id": "3m-aUuSnQ3A",
       "title": "Mare Gastro Sapanca’da Fujifilm Kamera ile retro anlar 📸❣️",
       "date": "12 Eyl 2026",
-      "views": 1080,
+      "views": 1084,
       "likes": 14,
       "comments": 1,
       "url": "https://youtu.be/3m-aUuSnQ3A"
